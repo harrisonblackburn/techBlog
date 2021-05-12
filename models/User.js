01a4a244207eda4b5c355bc318fd846cf70daa20
harrisonblackburn <blackbhr@yahoo.com>
2021-05-11 20:04:04 -0500

    fixed user js file to include correct attributes

--- models/User.js
+++ models/User.js
@@ -10,17 +10,7 @@ class User extends Model {
 
 User.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
