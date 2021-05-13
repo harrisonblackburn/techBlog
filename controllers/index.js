9f35cfcd74e0bb1daffc7f383316e1649150d165
harrisonblackburn <blackbhr@yahoo.com>
2021-05-12 21:24:36 -0500

    changed file folder structure; working on routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,27 +1,27 @@
-const router = require('express').Router();
-
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
-
-
-
-
-
-
-
 // const router = require('express').Router();
 
-// const postRouter = require('./api/postRoutes');
+// const apiRoutes = require('./api');
+// const homeRoutes = require('/homeRoutes');
 
-// const homeRouter = require('./homeRoutes');
+// router.use('/', homeRoutes);
+// router.use('./api', apiRoutes);
 
-// const userRouter = require('./api/userRoutes');
+// module.exports = router;
 
-// router.use('/api/user', userRouter)
-// // router.use('/api/post', postRouter)
-// router.use('/', homeRouter)
+
+
+
+
+
+
+const router = require('express').Router();
+
+const postRouter = require('./api/postRoutes');
+
+const homeRouter = require('./homeRoutes');
+
+const userRouter = require('./api/userRoutes');
+
+router.use('/api/user', userRouter)
+// router.use('/api/post', postRouter)
+router.use('/', homeRouter)
