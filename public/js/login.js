ecafe4d4e2fd5df06d1faf82c1ae7055fc39fc03
harrisonblackburn <blackbhr@yahoo.com>
2021-05-13 18:24:28 -0500

    ammended login js file

--- public/js/login.js
+++ public/js/login.js
@@ -15,40 +15,11 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
+        document.location.replace('/homepage');
       } else {
         alert(response.statusText);
       }
     }
   };
   
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
-  
+  document.querySelector("#login-form").addEventListener("submit", loginFormHandler);
